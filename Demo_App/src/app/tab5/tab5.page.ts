93b8466ef453939563aa03a5c236fcfcf80587e0
Bjarne <bjarne.huijs@student.ucll.be>
2019-05-22 09:35:22 +0200

    trying to connect frontend and backend

--- Demo_App/src/app/tab5/tab5.page.ts
+++ Demo_App/src/app/tab5/tab5.page.ts
@@ -13,23 +13,23 @@ export class Tab5Page {
   }
 
   async ionViewWillLoad() {
-    this.getItems();
+    /*this.getItems();
     for (const i of this.items) {
       console.log('works');
-    }
+    }*/
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SendAdviceMailPage');
+    console.log('ionViewDidLoad NewsFeed');
   }
 
   getItems() {
-    const options = {
-      headers: new HttpHeaders({'Content-type': 'application/json', 'Accept': 'application/json, */*'}),
+    /*const options = {
+      headers: new HttpHeaders({'Content-type': 'application/json', 'Accept': 'application/json, * /*'}),
     };
 
-    this.http.get('http://localhost/feed/', options).subscribe(data => {
+    this.http.get('http://localhost:8080/feed/', options).subscribe(data => {
       this.items = data;
-    });
+    });*/
   }
 }
